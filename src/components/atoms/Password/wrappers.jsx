7665c281b18905824072d72fc8a906855c6befe2
Deji <adeyemideji9@gmail.com>
2023-08-10 16:39:24 +0100

    refactored code

--- src/components/atoms/Password/wrappers.jsx
+++ src/components/atoms/Password/wrappers.jsx
@@ -114,8 +114,8 @@ export const InputWrapper = styled("div")(({ mquery, inputWidth, inputHeight, er
 	position: "relative",
 
 	input: {
-		width: `${inputWidth ? inputWidth : '100%'}`,
-		height: inputHeight ? inputHeight : "46px",
+		width: `${inputWidth != "" ? inputWidth : '100%'}`,
+		height: inputHeight != "" ? inputHeight : "46px",
 		borderRadius: "6px",
 		background: "#EEE",
 		padding: "15px",
