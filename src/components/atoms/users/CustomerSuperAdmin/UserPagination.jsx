f906d26292fa2db4ffcb34515d0e7dfd9c84d227
Richard Damilola <ogungbilerichard@gmail.com>
2023-08-18 15:53:16 +0100

    pagination sorted

--- src/components/atoms/users/CustomerSuperAdmin/UserPagination.jsx
+++ src/components/atoms/users/CustomerSuperAdmin/UserPagination.jsx
@@ -2,8 +2,46 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Typography } from '@mui/material';
 
-export default function PaginationRounded({ totalResults, resultsPerPage, currentPage, onPageChange }) {
+const ResultsInfo = ({ startIndex, endIndex, totalResults }) => (
+  <Typography
+    sx={{
+      fontSize: '#706E6E',
+      fontWeight: '400',
+      color: '#706E6E',
+      marginRight: '20px',
+    }}
+  >
+    Showing {startIndex} - {endIndex} of {totalResults} Results
+  </Typography>
+);
+
+const CustomPagination = ({ totalPages, currentPage, onPageChange }) => (
+  <Pagination
+    count={totalPages}
+    shape="rounded"
+    page={currentPage}
+    onChange={onPageChange}
+    sx={{
+      '& .MuiPaginationItem-root': {
+        color: '#2b2e72',
+        backgroundColor: 'transparent',
+        '&:hover': {
+          backgroundColor: '#2B2E72',
+          color: '#fff',
+        },
+        '&.Mui-selected': {
+          backgroundColor: '#2b2e72',
+          color: '#fff',
+        },
+      },
+    }}
+  />
+);
+
+const PaginationRounded = ({ totalResults, resultsPerPage, currentPage, onPageChange }) => {
   const totalPages = Math.ceil(totalResults / resultsPerPage);
+  const startIndex = (currentPage - 1) * resultsPerPage + 1;
+  const endIndex = Math.min(startIndex + resultsPerPage - 1, totalResults);
 
   const handlePageChange = (event, page) => {
     onPageChange(page);
@@ -22,36 +60,18 @@ export default function PaginationRounded({ totalResults, resultsPerPage, curren
         display: 'flex',
       }}
     >
-      <Typography
-        sx={{
-          fontSize: '#706E6E',
-          fontWeight: '400',
-          color: '#706E6E',
-          marginRight: '20px',
-        }}
-      >
-        Showing {resultsPerPage} of {totalResults} Results
-      </Typography>
-      <Pagination
-        count={totalPages}
-        shape="rounded"
-        page={currentPage}
-        onChange={handlePageChange}
-        sx={{
-          '& .MuiPaginationItem-root': {
-            color: '#2b2e72',
-            backgroundColor: 'transparent',
-            '&:hover': {
-              backgroundColor: '#2B2E72',
-              color: '#fff',
-            },
-            '&.Mui-selected': {
-              backgroundColor: '#2b2e72',
-              color: '#fff',
-            },
-          },
-        }}
+      <ResultsInfo
+        startIndex={startIndex}
+        endIndex={endIndex}
+        totalResults={totalResults}
+      />
+      <CustomPagination
+        totalPages={totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
       />
     </div>
   );
-}
+};
+
+export default PaginationRounded;
