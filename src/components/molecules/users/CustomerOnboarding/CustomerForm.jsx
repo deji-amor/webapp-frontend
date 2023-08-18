6541d313889c70dc44708a868557bb357849820e
Deji <adeyemideji9@gmail.com>
2023-08-18 13:40:12 +0100

    .

--- src/components/molecules/users/CustomerOnboarding/CustomerForm.jsx
+++ src/components/molecules/users/CustomerOnboarding/CustomerForm.jsx
@@ -244,7 +244,7 @@ const CustomerForm = ({ open, onClose }) => {
 								>
 									Cancel
 								</Button>
-								<CustomButton butText="Save And Send Link" loading={loading} butType={"submit"} style={{fontSize: "12px"}} />
+								<CustomButton butText="Save And Send Link" loading={loading} butType={"submit"} style={{fontSize: "8px", padding: "10px"}} />
 							</Box>
 						</form>
 					</DialogContent>
