4e9037ddbd4baeb0cbbd80bfd9db5273e08977ae
bube <attahgbube@gmail.com>
2023-08-22 21:57:56 +0100

    -

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,23 @@
-import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
-import svgrPlugin from "vite-plugin-svgr";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-	// https://github.com/vitejs/vite/issues/1973
-	define: {
-		...(import.meta.env?.NODE_ENV === "development" ? {global: "window"} : {}),
-	},
-	server: {
-		port: 3000,
-	},
-	resolve: {
-		// https://github.com/aws-amplify/amplify-js/issues/9639
-		alias: {
-			"./runtimeConfig": "./runtimeConfig.browser",
-		},
-	},
-	plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/setupTests.ts",
-		coverage: {
-			reporter: ["text", "html"],
-			exclude: ["node_modules/", "src/setupTests.ts"],
-		},
-	},
-	build: {
-		outDir: "build",
-		sourcemap: true,
-		commonjsOptions: {
-			include: [/node_modules/],
-			extensions: [".js", ".cjs"],
-			strictRequires: true,
-			// https://stackoverflow.com/questions/62770883/how-to-include-both-import-and-require-statements-in-the-bundle-using-rollup
-			transformMixedEsModules: true,
-		},
-	},
-};
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  }
+})
