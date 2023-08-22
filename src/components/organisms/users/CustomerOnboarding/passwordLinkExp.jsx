014bca04f201416d9943f3875e9c926af8b5b95a
Deji <adeyemideji9@gmail.com>
2023-08-22 15:55:02 +0100

    .

--- src/components/organisms/users/CustomerOnboarding/passwordLinkExp.jsx
+++ src/components/organisms/users/CustomerOnboarding/passwordLinkExp.jsx
@@ -15,65 +15,68 @@ const PasswordLinkExp = () => {
 
 	const { validationResponse } = useSelector((state) => state.customers);
 
+	console.log(validationResponse === "Your account has already been verified!");
+
 	const handleResendClick = (email) => {
 		dispatch(resendMyVerificationLink({ email }));
 	};
 
 	return (
-		<PasswordExpireWrapper width="550px">
-			{/* {validationResponse === "Your account has already been verified!" && ( */}
-				<ErrorCard
-					icon={<WarningAmberIcon className="icon" />}
-					backgroundColor={"#FFBF00"}
-					iconColor="white"
-					titleColor="white"
-					color="white"
-					title="Validation link used."
-					description="Your account has already been verified!"
-				/>
-			{/* )} */}
-			<PasswordSuccess
-				icon={<ErrorOutlineIcon className="icon" />}
-				title="Oops! Password Creation Link Has Expired."
-				iconSize="56px"
-				description="Please request another password creation link to complete your account activation."
-				padding={true}
-				titleSize="32px"
-				size="18px"
-				iconColor="#D73D3D"
-				desWidth="100%"
-				color="#828282"
+		<>
+			<ErrorCard
+				icon={<WarningAmberIcon className="icon" />}
+				backgroundColor="#FFBF00"
+				iconColor="white"
+				titleColor="white"
+				color="white"
+				title="Validation link used."
+				description="Your account has already been verified!"
 			/>
 
-			<div
-				style={{
-					display: "flex",
-					fontFamily: "Poppins",
-					textAlign: "center",
-					justifyContent: "center",
-				}}
-			>
-				<Button
-					onClick={() => handleResendClick(email)}
-					variant="contained"
-					sx={{
-						background: "#2b2e72",
+			<PasswordExpireWrapper width="550px">
+				<PasswordSuccess
+					icon={<ErrorOutlineIcon className="icon" />}
+					title="Oops! Password Creation Link Has Expired."
+					iconSize="56px"
+					description="Please request another password creation link to complete your account activation."
+					padding={true}
+					titleSize="32px"
+					size="18px"
+					iconColor="#D73D3D"
+					desWidth="100%"
+					color="#828282"
+				/>
+
+				<div
+					style={{
+						display: "flex",
 						fontFamily: "Poppins",
-						textTransform: "none",
-						padding: "10px 18px",
-						fontSize: "20px",
-						fontWeight: "600",
-						borderRadius: "10px",
-						width: "80%",
-						"&:hover": {
-							backgroundColor: "#2b2e72",
-						},
+						textAlign: "center",
+						justifyContent: "center",
 					}}
 				>
-					Request New Link
-				</Button>
-			</div>
-		</PasswordExpireWrapper>
+					<Button
+						onClick={() => handleResendClick(email)}
+						variant="contained"
+						sx={{
+							background: "#2b2e72",
+							fontFamily: "Poppins",
+							textTransform: "none",
+							padding: "10px 18px",
+							fontSize: "20px",
+							fontWeight: "600",
+							borderRadius: "10px",
+							width: "80%",
+							"&:hover": {
+								backgroundColor: "#2b2e72",
+							},
+						}}
+					>
+						Request New Link
+					</Button>
+				</div>
+			</PasswordExpireWrapper>
+		</>
 	);
 };
 
