097ccfe86bf2a502b2bb45f04d6ad106275924b6
bube <attahgbube@gmail.com>
2023-08-22 22:41:32 +0100

    tried alternate way of pushing documents to aws s3 storage using aws-sdk v3 or @aws-sdk/client-s3

--- src/aws/aws-crud-operations.js
+++ src/aws/aws-crud-operations.js
@@ -1,35 +1,40 @@
-import AWS from './aws-config'; // Import the AWS configuration
-import {v4} from "uuid";
+import s3Client from "./aws-config";
+import {PutObjectCommand, DeleteObjectCommand} from "@aws-sdk/client-s3";
 
-export const uploadFile = async (file) => {
-  const s3 = new AWS.S3();
+export const uploadFile = async file => {
+	const uniqueFileName = `file_${Date.now()}_${file.name}`.replaceAll(" ", "");
 
-  const params = {
-    Bucket: import.meta.env.VITE_NEXT_PUBLIC_APP_AWS_BUCKET_NAME,
-    Key: `${v4()}_${file.name}`,
-    Body: file,
-  };
+	const params = {
+		Bucket: import.meta.env.VITE_NEXT_PUBLIC_APP_AWS_BUCKET_NAME, // Replace with your S3 bucket name
+		Key: uniqueFileName,
+		Body: file,
+	};
 
-  try {
-    console.log('File uploaded successfully');
-    return await s3.upload(params).promise();
-  } catch (error) {
-    console.error('Error uploading file:', error);
-  }
+	try {
+		await s3Client.send(new PutObjectCommand(params));
+		console.log("File uploaded successfully");
+
+		// Construct the URL using the S3 bucket, region, and key (object name)
+		const fileUrl = `https://${params.Bucket}.s3.amazonaws.com/${params.Key}`;
+
+		return fileUrl; // Return the constructed URL
+		// return await s3Client.send(new PutObjectCommand(params));
+	} catch (error) {
+		console.error("Error uploading file:", error);
+	}
 };
 
 export const deleteFile = async (fileKey) => {
-  const s3 = new AWS.S3();
-
   const params = {
-    Bucket: import.meta.env.env.VITE_NEXT_PUBLIC_APP_AWS_BUCKET_NAME,
-    Key: fileKey,
-  };
+		Bucket: import.meta.env.VITE_NEXT_PUBLIC_APP_AWS_BUCKET_NAME, // Replace with your S3 bucket name
+		Key: fileKey,
+	};
 
   try {
-    console.log('File deleted successfully');
-    return await s3.deleteObject(params).promise();
+    return await s3Client.send(new DeleteObjectCommand(params));
   } catch (error) {
     console.error('Error deleting file:', error);
   }
 };
+
+export default deleteFile;
